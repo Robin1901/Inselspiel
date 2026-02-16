25c445044babf1679de67b63a156d27cc8674b60
Husky1411 <madler1411@gmail.com>
2026-02-16 20:28:12 +0100

    ,

--- Inselspiel/Assets/Scripts/Character/CharMove.cs
+++ Inselspiel/Assets/Scripts/Character/CharMove.cs
@@ -4,8 +4,10 @@ using UnityEngine.InputSystem;
 public class CharMove : MonoBehaviour
 {
     private float speed = 5f;
-    private float jumpHight = 2f;
-    private float gravity = -9.81f;
+    private float jumpHight = 1.5f;
+    private float gravity = -20f;
+    private float sprint = 10f;
+
 
     private CharacterController controller;
     private Vector2 moveInput;
@@ -16,18 +18,40 @@ public class CharMove : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
-           
+
     }
 
     public void onMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+
+    }
+
+    public void onJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHight * -1.5f * gravity);
+        }
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+
+        // sprint when Left Shift is held (only changes horizontal speed)
+        float currentSpeed = speed;
+        if (Keyboard.current != null && Keyboard.current.leftShiftKey.isPressed)
+        {
+            currentSpeed = sprint;
+        }
+
+        controller.Move(move * currentSpeed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
     }
 }
